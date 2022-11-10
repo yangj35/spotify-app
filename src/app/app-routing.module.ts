43d8297cefcdc6e19412002040a0e11d89821a4b
yangj35 <yangjal@umich.edu>
2022-11-10 00:46:38 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter, RouterConfigOptions } from '@angular/router';
+import { SearchComponent } from './home/search/search.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:SearchComponent },
+  { path: 'about', component: AboutComponent },
+];
+
+export const appRouterProviders = [
+  provideRouter(routes),
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
